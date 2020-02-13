958f5d260c6562b8e8c8bcb84cc4c3bafd5b91db
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2020-02-13 15:13:53 +0100

    Fixed formatting & app setup for vendors/express.js

--- vendors/app/express/express.js
+++ vendors/app/express/express.js
@@ -1,18 +1,15 @@
-var	path = require('path');
-var	express = require('express');
+var path = require('path');
+var express = require('express');
 
-module.exports.init = function(app,data_app,next)
-{
-  app.set('views',path.join(data_app.root, 'views'));
+module.exports.init = function (app, data_app, next) {
+  app.set('views', path.join(data_app.root, 'views'));
   app.set('view engine', 'ejs');
   app.use(express.static(path.join(data_app.root, 'public')));
-  if(data_app.config.http)
-  {
-  	app.set('port', data_app.config.http.port);
+  if (data_app.config.http) {
+    app.set('port', data_app.config.http.port);
   }
-  if(data_app.config.https)
-  {
-  	app.set('port_https', data_app.config.https.port);
+  if (data_app.config.https) {
+    app.set('port_https', data_app.config.https.port);
   }
   app.set('data_app', data_app);
   next();
